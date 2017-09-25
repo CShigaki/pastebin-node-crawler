2110d04cd014b647f77103d70a788a6fff228ee7
Celso Takeo Shigaki <celsomaker@gmail.com>
2017-09-25 16:26:40 -0300

    Adiciona occorencias no redis

--- master.js
+++ master.js
@@ -8,68 +8,37 @@ const redis = require("redis");
 const client = redis.createClient();
 
 const SEARCH_WORDS = ['carmen', 'help'];
-const PASTEBIN_RECENT_PASTES_API_URL = 'https://pastebin.com/api_scraping.php?limit=250';
+const PASTEBIN_RECENT_PASTES_API_URL = 'https://pastebin.com/api_scraping.php?limit=30';
 
-var wordGroup1 = ['lol', 'find', 'exit', 'code', 'function', 'Power'];
+var wordGroup1 = ['lol', 'find', 'exit', 'function', 'trump', 'help', 'person', 'guide', 'member', 'code'];
 var waitingTime = 10;
 
-if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`${worker.process.pid} exited.`);
-    if (!Object.keys(cluster.workers).length) {
-      process.exit(1);
-    }
-  });
+cluster.setupMaster({
+  exec: 'worker.js',
+})
 
-  axios.get(PASTEBIN_RECENT_PASTES_API_URL)
-    .then((result) => {
-      var pastesLinks = [];
+console.log(`Master ${process.pid} is running`);
 
+cluster.on('exit', (worker, code, signal) => {
+  console.log(`worker ${worker.process.pid} exiting...`);
+  if (!Object.keys(cluster.workers).length) {
+    process.exit(1);
+  }
+});
+
+axios.get(PASTEBIN_RECENT_PASTES_API_URL)
+  .then((result) => {
+    client.hkeys("visitedPastes", function (err, replies) {
       result.data.map((pasteInformation) => {
-        pastesLinks.push({ full_url: pasteInformation.full_url, scrape_url: pasteInformation.scrape_url });
+        if (replies.indexOf(pasteInformation.full_url) === -1) {
+          client.hmset("visitedPastes", pasteInformation.full_url, true);
+          cluster.fork({ KEYWORDS_TO_VERIFY: JSON.stringify(wordGroup1), PASTE_LINK: pasteInformation.scrape_url, WAITING_TIMEOUT: waitingTime });
+          sleep.msleep(waitingTime);
+          waitingTime += 500;
+        }
       });
-
-      for (let i = 0; i < numCPUs; i++) {
-        cluster.fork({ PASTES_LINKS: JSON.stringify(pastesLinks), WAITING_TIMEOUT: waitingTime});
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-} else {
-  console.log(`Worker ${process.pid} is running`);
-  const pastesLinks = JSON.parse(process.env.PASTES_LINKS);
-
-  sleep.msleep(process.env.WAITING_TIMEOUT);
-
-  client.hkeys("visitedPastes", (err, replies) => {
-    pastesLinks.map((pasteLinks) => {
-      if (replies.indexOf(pasteLinks.full_url) === -1) {
-        client.hmset("visitedPastes", pasteLinks.full_url, true);
-        waitingTime += 166;
-
-        client.get('occurences', (err, reply) => {
-          var occurences = JSON.parse(reply);
-
-          axios.get(pasteLinks.scrape_url)
-            .then((result) => {
-              const pageContent = result.data;
-
-              wordGroup1.map((keyword) => {
-                const regexString = `/${keyword}/`;
-                const regex = new RegExp(regexString, 'gi');
-
-                if (pageContent.match(regex)) {
-                  console.log(`achou ${keyword}`);
-                  occurences.push({ keyword: keyword, uri: pasteLinks.scrape_url });
-                }
-              });
-
-              client.set('occurences', JSON.stringify(occurences));
-            });
-        })
-      }
     });
-  });
-}
+  })
+  .catch((error) => {
+    console.log(error);
+  })
