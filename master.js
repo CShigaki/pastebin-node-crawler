761a4dd4853ac18e30b025fe6c6b547ae10be6d3
Celso Takeo Shigaki <celsomaker@gmail.com>
2017-09-25 17:01:06 -0300

    Adiciona clusterização com armazenamento de ocorrencias

--- master.js
+++ master.js
@@ -8,35 +8,52 @@ const redis = require("redis");
 const client = redis.createClient();
 
 const SEARCH_WORDS = ['carmen', 'help'];
-const PASTEBIN_RECENT_PASTES_API_URL = 'https://pastebin.com/api_scraping.php?limit=30';
+const PASTEBIN_RECENT_PASTES_API_URL = 'https://pastebin.com/api_scraping.php?limit=100';
 
 var wordGroup1 = ['lol', 'find', 'exit', 'function', 'trump', 'help', 'person', 'guide', 'member', 'code'];
 var waitingTime = 10;
 
-cluster.setupMaster({
-  exec: 'worker.js',
-})
-
-console.log(`Master ${process.pid} is running`);
-
-cluster.on('exit', (worker, code, signal) => {
-  console.log(`worker ${worker.process.pid} exiting...`);
-  if (!Object.keys(cluster.workers).length) {
-    process.exit(1);
-  }
-});
-
 axios.get(PASTEBIN_RECENT_PASTES_API_URL)
   .then((result) => {
     client.hkeys("visitedPastes", function (err, replies) {
+      var pastesLinks = [];
+
       result.data.map((pasteInformation) => {
         if (replies.indexOf(pasteInformation.full_url) === -1) {
+          pastesLinks.push(pasteInformation.full_url);
           client.hmset("visitedPastes", pasteInformation.full_url, true);
-          cluster.fork({ KEYWORDS_TO_VERIFY: JSON.stringify(wordGroup1), PASTE_LINK: pasteInformation.scrape_url, WAITING_TIMEOUT: waitingTime });
-          sleep.msleep(waitingTime);
-          waitingTime += 500;
         }
       });
+
+      pastesLinks.map((pasteLink) => {
+        sleep.msleep(waitingTime);
+        waitingTime += 500;
+        axios.get(pasteLink)
+          .then((result) => {
+            console.log(`${pasteLink} - ${result.status}`);
+            const rawContent = result.data;
+
+            client.get('occurences', (err, reply) => {
+              var occurences = JSON.parse(reply);
+
+              if (!occurences) {
+                occurences = [];
+              }
+
+              wordGroup1.map((keyword) => {
+                const regex = new RegExp(keyword, 'gi');
+
+                if (rawContent.match(regex)) {
+                  console.log(`Achou! Link: ${pasteLink}, Keyword: ${keyword}`);
+                  occurences.push({ keyword: keyword, uri: pasteLink })
+                }
+                client.set('occurences', JSON.stringify(occurences));
+              });
+
+              console.log(`Finished processing paste ${pasteLink}. Killing this worker.`);
+            });
+          });
+      })
     });
   })
   .catch((error) => {
--- worker.js
+++ worker.js
@@ -3,34 +3,36 @@ const sleep = require('sleep');
 const redis = require("redis");
 const client = redis.createClient();
 
-//console.log(`Worker will take care of paste ${process.env.PASTE_LINK}`);
-const pasteToVerify = process.env.PASTE_LINK;
+// console.log(`Worker will take care of paste ${process.env.PASTE_LINK}`);
+const pastesToVerify = JSON.parse(process.env.PASTES_LINKS);
 const keywordsToVerify = JSON.parse(process.env.KEYWORDS_TO_VERIFY);
 
-sleep.msleep(process.env.WAITING_TIMEOUT);
-axios.get(pasteToVerify)
-  .then((result) => {
-    console.log(result.status)
-    const rawContent = result.data;
+// sleep.msleep(process.env.WAITING_TIMEOUT);
+pastesToVerify.map((pasteLink) => {
+  axios.get(pasteLink)
+    .then((result) => {
+      console.log(result.status)
+      const rawContent = result.data;
 
-    client.get('occurences', (err, reply) => {
-      var occurences = JSON.parse(reply);
+      client.get('occurences', (err, reply) => {
+        var occurences = JSON.parse(reply);
 
-      if (!occurences) {
-        occurences = [];
-      }
-
-      keywordsToVerify.map((keyword) => {
-        const regex = new RegExp(keyword, 'gi');
-
-        if (rawContent.match(regex)) {
-          console.log(`Achou! Link: ${pasteToVerify}, Keyword: ${keyword}`);
-          occurences.push({ keyword: keyword, uri: pasteToVerify })
-          client.set('occurences', JSON.stringify(occurences));
+        if (!occurences) {
+          occurences = [];
         }
-      });
 
-      console.log(`Finished processing paste ${pasteToVerify}. Killing this worker.`);
-      process.exit(1);
+        keywordsToVerify.map((keyword) => {
+          const regex = new RegExp(keyword, 'gi');
+
+          if (rawContent.match(regex)) {
+            console.log(`Achou! Link: ${pasteLink}, Keyword: ${keyword}`);
+            occurences.push({ keyword: keyword, uri: pasteLink })
+            client.set('occurences', JSON.stringify(occurences));
+          }
+        });
+
+        console.log(`Finished processing paste ${pasteLink}. Killing this worker.`);
+        process.exit(1);
+      });
     });
-  });
+})
