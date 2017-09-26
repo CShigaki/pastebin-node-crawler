42e103c66c32ed2da0ba4a06c03b4cb2b5ba1752
Celso Takeo Shigaki <celsomaker@gmail.com>
2017-09-26 11:22:51 -0300

    Adiciona ecosystem para pm2 e corrige timeout para evitar block de ip

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,8 @@ module.exports = {
   apps : [{
     name: "pastebin-node-crawler-master",
     script: "./master.js",
-    instances: 1,
-    exec_mode: "fork"
+    instances: 4,
+    exec_mode: "cluster",
+    cron_restart: "* * * * *"
   }]
 }
--- master.js
+++ master.js
@@ -8,7 +8,7 @@ const redis = require("redis");
 const client = redis.createClient();
 
 const SEARCH_WORDS = ['carmen', 'help'];
-const PASTEBIN_RECENT_PASTES_API_URL = 'https://pastebin.com/api_scraping.php?limit=100';
+const PASTEBIN_RECENT_PASTES_API_URL = 'https://pastebin.com/api_scraping.php?limit=250';
 
 var wordGroup1 = ['lol', 'find', 'exit', 'function', 'trump', 'help', 'person', 'guide', 'member', 'code'];
 var waitingTime = 10;
@@ -21,38 +21,36 @@ axios.get(PASTEBIN_RECENT_PASTES_API_URL)
       result.data.map((pasteInformation) => {
         if (replies.indexOf(pasteInformation.full_url) === -1) {
           pastesLinks.push(pasteInformation.full_url);
-          client.hmset("visitedPastes", pasteInformation.full_url, true);
+          client.hmset("visitedPastes", pasteInformation.full_url, true, redis.print);
         }
       });
 
       pastesLinks.map((pasteLink) => {
-        sleep.msleep(waitingTime);
-        waitingTime += 500;
-        axios.get(pasteLink)
-          .then((result) => {
-            console.log(`${pasteLink} - ${result.status}`);
-            const rawContent = result.data;
+        setTimeout(() => {
+          axios.get(pasteLink)
+            .then((result) => {
+              console.log(`${pasteLink} - ${result.status}`);
+              const rawContent = result.data;
 
-            client.get('occurences', (err, reply) => {
-              var occurences = JSON.parse(reply);
-
-              if (!occurences) {
-                occurences = [];
-              }
-
-              wordGroup1.map((keyword) => {
-                const regex = new RegExp(keyword, 'gi');
-
-                if (rawContent.match(regex)) {
-                  console.log(`Achou! Link: ${pasteLink}, Keyword: ${keyword}`);
-                  occurences.push({ keyword: keyword, uri: pasteLink })
+              client.get('occurences', (err, reply) => {
+                var occurences = JSON.parse(reply);
+                if (!occurences) {
+                  occurences = [];
                 }
-                client.set('occurences', JSON.stringify(occurences));
-              });
 
-              console.log(`Finished processing paste ${pasteLink}. Killing this worker.`);
+                wordGroup1.map((keyword) => {
+                  const regex = new RegExp(keyword, 'gi');
+
+                  if (rawContent.match(regex)) {
+                    console.log(`Found new occurence! Link: ${pasteLink}, Keyword: ${keyword}`);
+                    occurences.push({ keyword: keyword, uri: pasteLink })
+                  }
+                  client.set('occurences', JSON.stringify(occurences));
+                });
+              });
             });
-          });
+        }, waitingTime);
+        waitingTime += 300;
       })
     });
   })
